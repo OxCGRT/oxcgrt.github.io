b7210fad19338f77834ef46cc44c6562b0faa743
Toby Phillips <59369581+TobyPhillips@users.noreply.github.com>
2022-01-24 22:11:57 +1030

    add here() for finding Red

--- r-code/changes-render.R
+++ r-code/changes-render.R
@@ -2,4 +2,4 @@ library(rmarkdown)
 library(here)
 
 here()
-render("r-code/ChangesMarkdown.Rmd", output_file = "../_pages/recent-changes.md")
+render(here("ChangesMarkdown.Rmd"), output_file = "../_pages/recent-changes.md")
